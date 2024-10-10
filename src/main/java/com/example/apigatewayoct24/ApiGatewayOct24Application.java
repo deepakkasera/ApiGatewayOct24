44ce26c15ad4319d00214f9c22d6faacd8cc8b9e
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-10-10 08:29:10 +0530

    API GW

---
+++ src/main/java/com/example/apigatewayoct24/ApiGatewayOct24Application.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayoct24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayOct24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayOct24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apigatewayoct24/ApiGatewayOct24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayoct24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayOct24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
